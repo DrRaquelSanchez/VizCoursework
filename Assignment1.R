b6e1e36d608eeb07d2b3884f3a3e32489e31a034
DrRaquelSanchez <97776796+DrRaquelSanchez@users.noreply.github.com>
2022-01-18 10:43:12 -0800

    Update Assignment1.R

--- Assignment1.R
+++ Assignment1.R
@@ -1,15 +1,9 @@
-### Project:
+### Project 1:
 
-Prepare a plot of one categorical variable of your choosing. 
-If you do not have one yet, practise the next exercise:
-  
 The variable "Free Lunch" from the table used in this session is telling you how many students receive lunch in a school. 
-If you request a summary you get:
-  ```{r, eval=TRUE}
-summary(eduwa$Free.Lunch)
-```
 
-Then, when you see the minimum and max value, you can create groups by **cutting** the variable:
+summary(eduwa$Free.Lunch)
+#Then, when you see the minimum and max value, you can create groups by **cutting** the variable:
   
   ```{r, eval=TRUE}
 eduwa$Free.LunchGroup=cut(eduwa$Free.Lunch,
@@ -21,15 +15,16 @@ This new variable has this table:
   ```{r, eval=TRUE}
 table(eduwa$Free.LunchGroup)
 
-
-```
-
 Prepare a plot using the table above. 
 
 #turn the table into a data frame
 absoluteT2=table(eduwa$Free.LunchGroup,
                 exclude = 'nothing') 
 View(absoluteT2)
+
+#re-naming the n/a values
+
+names(absoluteT2)[6]='Unknown'
 
 (tableFreq2=as.data.frame(absoluteT2))
 
@@ -41,10 +36,6 @@ names(tableFreq2)=c("FreeLunch","Count")
 tableFreq2$Percent=as.vector(propT2)
 
 tableFreq2
-
-#re-naming the n/a values
-
-names(absoluteT2)[6]='Unknown'
 
 #range as percents 
 
@@ -61,7 +52,7 @@ base2= ggplot(data = tableFreq2, aes(x = FreeLunch, y = Percent))
 #add the layer that produces the main plots 
 #(the next layers will add or customize elements in the plot):
 
-plot7 = base2 + geom_bar(fill ="gray",
+plot7 = base2 + geom_bar(fill ="blue",
                         stat = 'identity') 
 plot7
 
@@ -69,7 +60,7 @@ titleText='Total Students Receiving Free Lunch by School'
 sourceText='Source: US Department of Education'
 
 plot8 = plot7 + labs(title=titleText,
-                     x = "# of Students", 
+                     x = "Ranges In Schools", 
                      y = "% of Students per School",
                      caption = sourceText) 
 plot8
@@ -90,6 +81,14 @@ plot11 = plot10 + theme(plot.caption = element_text(hjust = 0),
                       plot.title = element_text(hjust = 0.5))
 plot11
 
+tableFreq2=tableFreq2[order(tableFreq2$Percent),]
+# then:
+tableFreq2
+(FreeLunchOrder=tableFreq2[order(tableFreq2$Percent),'FreeLunchOrder'])
+LABELS=paste0(round(tableFreq2$Percent,2), '%')
+base2= ggplot(data = tableFreq2, aes(x = FreeLunchOrder, y = Percent))
+
+
 paste0(round(tableFreq2$Percent,2), '%')
 
 # createing labels:
@@ -99,15 +98,12 @@ plot12 = plot11 + geom_text(vjust=0, #hjust if flipping
                           size = 6,
                           aes(y = Percent ,
                               label = LABELS))
-plot12 # + coord_flip() # wanna flip the plot?
+plot12 
+# + coord_flip() # wanna flip the plot?
 
-tableFreq2=tableFreq2[order(tableFreq2$Percent),]
-# then:
-tableFreq2
-(FreeLunchGroup=tableFreq2[order(tableFreq2$Percent),'FreeLunch'])
-LABELS=paste0(round(tableFreq2$Percent,2), '%')
-base2= ggplot(data = tableFreq2, aes(x = FreeLunch, y = Percent))
-## adding these to base
+
+
+#### adding these to base###
 base= base + scale_x_discrete(limits=FreeLunchGroup) 
 base= base + theme_classic()
 
@@ -119,7 +115,7 @@ titleText='Total Students Receiving Free Lunch by School'
 sourceText='Source: US Department of Education'
 
 plot8 = plot7 + labs(title=titleText,
-                     x = "# of Students", 
+                     x = "Ranges in Schools", 
                      y = "% of Students per School",
                      caption = sourceText) 
 plot8
@@ -130,7 +126,6 @@ plot9 = plot8 + geom_hline(yintercept = 25, #where
                            alpha=0.5) #transparency
 plot9
 
-# customize Y axis
 plot10 = plot9 + scale_y_continuous(breaks=c(0,10,20, 30,40),
                                     limits = c(0, 40), 
                                     labels=unit_format(suffix = '%')) 
@@ -149,13 +144,5 @@ LABELS=paste0(round(tableFreq2$Percent,2), '%')
 plot12 = plot11 + geom_text(vjust=0, size = 6, aes(y = Percent,label = LABELS))
 
 plot12
-  
 
-
-(FreeLunchGroup=tableFreq2[order(tableFreq2$Percent),'FreeLunch'])
-
-
-
-_____
-[Go to table of contents.](#part1)
   
